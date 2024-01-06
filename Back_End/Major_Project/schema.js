07c4a7e93332335ea154bc94ae185b286b1c5881
akashdeep023 <ad3500476@gmail.com>
2024-01-06 22:20:38 +0530

    update and add some file

--- Back_End/Major_Project/schema.js
+++ Back_End/Major_Project/schema.js
@@ -7,24 +7,33 @@ const Joi = require ("joi");
 
 //listings validation-------
 module.exports.listingSchema = Joi.object({
-    listing: Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        country: Joi.string().required(),
-        location: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().allow("",null),
-        category: Joi.allow(""),
-    }).required()//---------.required()     delete kiye to work kiya par 
-})
-//reviews validation-------
+	listing: Joi.object({
+		title: Joi.string().required(),
+		description: Joi.string().required(),
+		country: Joi.string().required(),
+		location: Joi.string().required(),
+		price: Joi.number().required().min(0),
+		image: Joi.string().allow("", null),
+		category: Joi.allow(""),
+	}).required(),
+});
+
+//review validation-------
 module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
-        comment: Joi.string().required(),
-        // createAt: Joi.date().allow("",null),
-    }).required()
-})
+	review: Joi.object({
+		rating: Joi.number().required().min(1).max(5),
+		comment: Joi.string().required(),
+	}).required(),
+});
+
+//user validation-------
+module.exports.userSchema = Joi.object({
+    fName: Joi.string().required().trim(),
+    lName: Joi.string().required().trim(),
+    username: Joi.string().required(),
+	email: Joi.string().required().email(),
+    password: Joi.string().min(8),
+});
 
 
 
